ee2fe1f9d5d5d8479966088ef4b78eb4ed0545bb
bielsnohr <matthew.bluteau@gmail.com>
2022-12-03 18:26:08 +0000

    Solve Day01 of Year2022

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Advent of Code solutions in Elm
 
-Theese are my attempts to solve advent of code in Elm. Feedback on solutions is welcome.
-
-You can test my solutions at https://albertdahlin.com/advent-of-code
-
+These are my attempts to solve advent of code in Elm. Feedback on solutions is welcome.
--- src/Year2022/Day01.elm
+++ src/Year2022/Day01.elm
@@ -8,8 +8,8 @@ import Util.Parser
 
 solution =
     { solve = solve
-    , title = "Sonar Sweep"
-    , subtitle = "Scan the sea floor and filter the data."
+    , title = "Calorie Counting"
+    , subtitle = "Calculate some metrics to quantify the calories the elves are carrying."
     , tests = []
     , performance = Performance.Acceptable
     }
@@ -20,15 +20,32 @@ solve input =
     let
         numbers =
             input
-                |> Parser.run (Util.Parser.parseRowsUsing Parser.int)
-                |> Result.mapError Util.Parser.firstErrorMsg
+                |> String.split "\n\n"
+                |> List.map (Parser.run <| Util.Parser.parseRowsUsing Parser.int)
+                |> List.map (Result.mapError Util.Parser.firstErrorMsg)
 
         r1 =
-            Err "-empty-"
+            numbers
+            |> sumCaloriesCarried
+            |> List.maximum
+            |> Result.fromMaybe "No maximum found"
+            |> Result.map String.fromInt
 
         r2 =
-            Err "-empty-"
+            numbers
+            |> sumCaloriesCarried
+            |> List.sort
+            |> List.reverse
+            |> List.take 3
+            |> List.sum
+            |> String.fromInt
+            -- I'm not sure how to handle the number to Result conversion here...
+            |> Ok
     in
     ( r1
     , r2
     )
+
+sumCaloriesCarried : List (Result String (List Int)) -> List Int
+sumCaloriesCarried snacksPerElf =
+    snacksPerElf |> List.map (Result.withDefault [0] >> List.sum)
