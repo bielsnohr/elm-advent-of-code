8203b8abbd179120c9c82b23dea12cb055788b14
bielsnohr <matthew.bluteau@gmail.com>
2022-12-19 16:06:52 +0000

    Format Day05 Year2022 test file.

--- tests/TestYear2022/Day05.elm
+++ tests/TestYear2022/Day05.elm
@@ -2,11 +2,11 @@ module TestYear2022.Day05 exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
-import Year2022.Day05 exposing (..)
-import Parser exposing (Parser)
-import Util.Parser
 import List.Extra exposing (transpose)
+import Parser exposing (Parser)
+import Test exposing (..)
+import Util.Parser
+import Year2022.Day06 exposing (..)
 
 
 test_stackParser : Test
@@ -14,51 +14,62 @@ test_stackParser =
     test "Verify the initial stack state parser is working." <|
         \_ ->
             let
-                stack_line = "    [A]    [B] [C]"
+                stack_line =
+                    "    [A]    [B] [C]"
             in
             stack_line
                 |> Parser.run stackParser
-                |> Expect.equal (Ok ["", "A", "", "B", "C"])
+                |> Expect.equal (Ok [ "", "A", "", "B", "C" ])
+
 
 test_stackParser_with_indices : Test
 test_stackParser_with_indices =
     test "Verify the stack parser works with the index line." <|
         \_ ->
             let
-                stack_line = " 1  2  3 "
+                stack_line =
+                    " 1  2  3 "
             in
             stack_line
                 |> Parser.run stackParser
                 |> Expect.equal (Ok [])
+
 
 test_transpose : Test
 test_transpose =
     test "Verify the transpose operation used on the input stack." <|
         \_ ->
             let
-                input_stacks = [["", "A", ""], ["B", "C", "D"]]
+                input_stacks =
+                    [ [ "", "A", "" ], [ "B", "C", "D" ] ]
             in
             input_stacks
                 |> transpose
-                |> Expect.equal [["", "B"], ["A", "C"], ["", "D"]]
+                |> Expect.equal [ [ "", "B" ], [ "A", "C" ], [ "", "D" ] ]
+
 
 test_removeEmptyStrings : Test
 test_removeEmptyStrings =
     test "Verify that empty string elements are removed from a list of lists." <|
         \_ ->
             let
-                input_stacks = [["A", "", ""], ["B", "C", ""]]
+                input_stacks =
+                    [ [ "A", "", "" ], [ "B", "C", "" ] ]
             in
             input_stacks
                 |> removeEmptyStrings
-                |> Expect.equal [["A"], ["B", "C"]]
+                |> Expect.equal [ [ "A" ], [ "B", "C" ] ]
+
 
 test_instructionParser =
     test "Verify that we can parse the crate moving instructions." <|
         \_ ->
             let
-                instruction = "move 1 from 2 to 3"
-                move_instruction = MoveInstruction 1 1 2
+                instruction =
+                    "move 1 from 2 to 3"
+
+                move_instruction =
+                    MoveInstruction 1 1 2
             in
             instruction
                 |> Parser.run instructionsParser
@@ -70,20 +81,29 @@ test_applyInstruction =
     test "Verify instructions are applied properly to stacks." <|
         \_ ->
             let
-                input_stacks = [["D", "N", "Z"], ["C", "M"], ["P"]]
-                move_instruction = createMoveInstruction 3 1 3
-                output_stacks = [[], ["C", "M"], ["Z", "N", "D", "P"]]
+                input_stacks =
+                    [ [ "D", "N", "Z" ], [ "C", "M" ], [ "P" ] ]
+
+                move_instruction =
+                    createMoveInstruction 3 1 3
+
+                output_stacks =
+                    [ [], [ "C", "M" ], [ "Z", "N", "D", "P" ] ]
             in
             applyInstruction move_instruction input_stacks
                 |> Expect.equal output_stacks
+
 
 test_getTopCrates : Test
 test_getTopCrates =
     test "Verify the top crates of each stack is retrieved." <|
         \_ ->
             let
-                input_stacks = [["D", "N", "Z"], ["C", "M"], ["P"]]
-                top_crates = "DCP"
+                input_stacks =
+                    [ [ "D", "N", "Z" ], [ "C", "M" ], [ "P" ] ]
+
+                top_crates =
+                    "DCP"
             in
             getTopCrates input_stacks
                 |> Expect.equal top_crates
