ef0aef03b13be486b6053f2f09bc271a7ada0c15
bielsnohr <matthew.bluteau@gmail.com>
2023-02-04 16:56:35 +0000

    Create netlify deployment files

--- notes.md
+++ notes.md
@@ -23,3 +23,31 @@
   `Debug.log` statements, which then pop up in the Debug console of the browser
   in which the program is run. *It doesn't come out of the console output!* Took
   me a while to figure that out.
+
+### Deployment
+
+Very little guidance out there on how to actually deploy an Elm application. Nor
+any indication that the most standard Elm architecture is a SPA (Single Page
+Application).
+
+After a lot of stumbling around, I eventually ended up with what I think is a
+way forward, involving these components.
+
+- The way to build with just elm is `elm make src/Main.elm --optimize
+  --output=elm.js`
+- [rtfeldman-elm-example] adds another step involving Uglify, but I don't this
+  is particularly necessary for my simple use case (???)
+- then, I found the simple `index.html` that loads this from the base template
+  that `elm-spa` produces
+- finally, I will need to connect this up to Netlify using the guidance at [elm-land]
+
+This takes inspiration mostly from these sources:
+
+- [rtfeldman-elm-example]
+- [elm-spa-tool]
+- [elm-land]
+
+
+[rtfeldman-elm-example]: https://github.com/rtfeldman/elm-spa-example#building
+[elm-spa-tool]: https://www.elm-spa.dev/guide
+[elm-land]: https://elm.land/guide/deploying.html
--- src/Year2022/Day03.elm
+++ src/Year2022/Day03.elm
@@ -1,13 +1,14 @@
 -- Use this as a base for daily solutions
+
+
 module Year2022.Day03 exposing (..)
 
+import List.Extra as E
+import Parser exposing ((|.), (|=), Parser)
 import Performance exposing (Performance)
 import Result.Extra as Result
-import Parser exposing ((|.), (|=), Parser)
-import Util.Parser
 import Set
-import List.Extra as E
-import Debug as Dbg
+import Util.Parser
 
 
 solution =
@@ -31,64 +32,84 @@ solve input =
             case rucksack_strings of
                 Ok strings ->
                     Ok (List.map calculateRucksackPriority strings |> List.sum |> String.fromInt)
+
                 Err err ->
                     Err err
 
         r2 =
             case rucksack_strings of
-                Ok strings -> 
-                    Ok (List.map calculateGroupPriority (E.groupsOf 3 strings)
-                        |> List.sum
-                        |> String.fromInt)
-                Err err -> Err err
+                Ok strings ->
+                    Ok
+                        (List.map calculateGroupPriority (E.groupsOf 3 strings)
+                            |> List.sum
+                            |> String.fromInt
+                        )
+
+                Err err ->
+                    Err err
     in
     ( r1
     , r2
     )
 
+
 calculateRucksackPriority : String -> Int
 calculateRucksackPriority rucksack =
     let
-        wrong_item = 
+        wrong_item =
             findWrongItem rucksack
     in
+    case wrong_item of
+        Just item ->
+            itemPriority item
 
-        case wrong_item of
-            Just item -> itemPriority item
-            Nothing -> 0
-        
-itemPriority : Char -> Int    
+        Nothing ->
+            0
+
+
+itemPriority : Char -> Int
 itemPriority item =
     if Char.isUpper item then
-        (Char.toCode item) - 38
+        Char.toCode item - 38
+
     else
-        (Char.toCode item) - 96
-    
+        Char.toCode item - 96
+
+
 findWrongItem : String -> Maybe Char
 findWrongItem rucksack =
     let
-        length = (String.length rucksack) // 2
-        item_types_1 = String.left length rucksack
-                    |> String.toList
-                    |> Set.fromList
-        item_types_2 = String.right length rucksack
-                    |> String.toList
-                    |> Set.fromList
+        length =
+            String.length rucksack // 2
+
+        item_types_1 =
+            String.left length rucksack
+                |> String.toList
+                |> Set.fromList
+
+        item_types_2 =
+            String.right length rucksack
+                |> String.toList
+                |> Set.fromList
     in
-        Set.intersect item_types_1 item_types_2 |> Set.toList |> List.head
+    Set.intersect item_types_1 item_types_2 |> Set.toList |> List.head
+
 
 calculateGroupPriority : List String -> Int
 calculateGroupPriority elves =
     let
-        sets = elves 
-            |> List.map (String.toList >> Set.fromList) 
-        badge = List.foldl Set.intersect (Maybe.withDefault Set.empty (List.head sets)) sets
-            |> Set.toList
-            |> List.head
-        
-        _ = Dbg.log "badge" badge
-    in
-        case badge of
-            Just bdg -> itemPriority bdg
-            Nothing -> 0
+        sets =
+            elves
+                |> List.map (String.toList >> Set.fromList)
 
+        badge =
+            List.foldl Set.intersect (Maybe.withDefault Set.empty (List.head sets)) sets
+                |> Set.toList
+                |> List.head
+    in
+    case badge of
+        Just bdg ->
+            itemPriority bdg
+
+        Nothing ->
+            0
